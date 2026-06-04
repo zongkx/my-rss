1940acec14748200e847ed55928a729509abb7b0
zongkx <1101732819@qq.com>
2026-06-04 14:31:53 +0800

    Update fetch_rss.js

--- fetch_rss.js
+++ fetch_rss.js
@@ -12,7 +12,6 @@ const turndownService = new TurndownService({
 // ================= 配置区域 =================
 const RSS_FEEDS = [
     'https://sspai.com/feed',
-    'https://www.v2ex.com/index.xml',
     'https://rss.huxiu.com',
     'https://www.infoq.cn/feed',
 ];
